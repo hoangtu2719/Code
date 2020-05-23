22b28180f5f32cb38b9728aa5030a7d9d0273007
Tu Luu <55480631+hoangtu2719@users.noreply.github.com>
2020-05-23 10:23:15 +0700

    Dangnhap

--- EducationCode/Controllers/NguoiDungController.cs
+++ EducationCode/Controllers/NguoiDungController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EducationCode.Models;
 
 namespace EducationCode.Controllers
 {
     public class NguoiDungController : Controller
     {
+        CodeEntities db = new CodeEntities();
         // GET: NguoiDung
         public ActionResult NguoiDung()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection f)
+        {
+            String taikhoan = f["txttaikhoan"].ToString();
+            String matkhau = f["txtmatkhau"].ToString();
+            NGUOIDUNG nd = db.NGUOIDUNGs.SingleOrDefault(n => n.TenNguoiDung == taikhoan && n.MatKhau == matkhau);
+            if(nd != null)
+            {
+                Session["NguoiDung"] = nd;
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+            else
+            {
+                ViewBag.ThongBao = "Dang nhap that bai"
+            }
+        }
+
     }
 }
