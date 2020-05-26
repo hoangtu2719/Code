c31dd897f8c0805f00855798398cd170bfb40eee
Tu Luu <55480631+hoangtu2719@users.noreply.github.com>
2020-05-26 20:42:46 +0700

    20h:42

--- EducationCode/Controllers/MenuController.cs
+++ EducationCode/Controllers/MenuController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EducationCode.Models;
 
 namespace EducationCode.Controllers
 {
     public class MenuController : Controller
     {
         // GET: Menu
+        DataCodeSharkEntities db = new DataCodeSharkEntities();
         public PartialViewResult Menu()
         {
             return PartialView();
+        }
+        public PartialViewResult DanhSachKhoaHoc()
+        {
+            return PartialView(db.tbKHOAHOCs.ToList());
         }
     }
 }
