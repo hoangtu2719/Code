97d59fd9ca96a7862a2d5a3d71129d5326035e04
Tu Luu <55480631+hoangtu2719@users.noreply.github.com>
2020-06-15 16:04:31 +0700

    Back end Login

--- EducationCode/Controllers/NguoiDung/DangKiController.cs
+++ EducationCode/Controllers/NguoiDung/DangKiController.cs
@@ -17,15 +17,27 @@ namespace EducationCode.Controllers.NguoiDung
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Dangki([Bind(Include = "MANGUOIDUNG,TAIKHOAN,MATKHAU,TENDANGNHAP,EMAIL,DIACHI,SOCMND,SODIENTHOAI")] NGUOIDUNG nGUOIDUNG)
+        public ActionResult Dangki([Bind(Include = "MANGUOIDUNG,TAIKHOAN,MATKHAU,TENDANGNHAP,EMAIL,DIACHI,SOCMND,SODIENTHOAI")] NGUOIDUNG nGUOIDUNG , String nhaplaimk,string taikhoan)
         {
             if (ModelState.IsValid)
             {
+                //Kiểm tra mật khảu có trùng khớp
+                if(nhaplaimk != nGUOIDUNG.MATKHAU )
+                {
+                    ViewBag.ThongBao = "Không trùng khớp";
+                    return View();
+                }
+                //Kiểm tra mật khảu có trùng khớp
+                NGUOIDUNG nd = db.NGUOIDUNGs.SingleOrDefault(n => n.TAIKHOAN == nGUOIDUNG.TAIKHOAN);
+                if( nd!=null )
+                {
+                    ViewBag.TaiKhoan = "Tài khoản đã tồn tại";
+                    return View();
+                }    
                 db.NGUOIDUNGs.Add(nGUOIDUNG);
                 db.SaveChanges();
                 return Redirect("/DangKi/DangKiThanhCong");
             }
-
             return View(nGUOIDUNG);
         }
         public ActionResult DangKiThanhCong()
--- EducationCode/Controllers/NguoiDung/DangNhapController.cs
+++ EducationCode/Controllers/NguoiDung/DangNhapController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using EducationCode.Models;
 
 namespace EducationCode.Controllers.NguoiDung
 {
