411dc986d22b92fc2496bdb6ca175b3ec77ba20a
Tu Luu <55480631+hoangtu2719@users.noreply.github.com>
2020-07-02 19:59:20 +0700

    Add module BinhLuan

---
+++ EducationCode/Controllers/BinhLuanController.cs
@@ -0,0 +1,24 @@
+﻿using EducationCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EducationCode.Controllers
+{
+    public class BinhLuanController : Controller
+    {
+        CODESHARKEntities db = new CODESHARKEntities();
+        // GET: BinhLuan
+        public ActionResult NhapBinhLuan(BINHLUAN bl)
+        {
+            NGUOIDUNG nd = (NGUOIDUNG)Session["NguoiDung"];
+            bl.NGAYDANG = DateTime.Now;
+            bl.MANGUOIDUNG = nd.MANGUOIDUNG;
+            db.BINHLUANs.Add(bl);
+            db.SaveChanges();
+            return Redirect("/TrangChu/Index");
+        }
+    }
+}
--- EducationCode/Controllers/TimKiemController.cs
+++ EducationCode/Controllers/TimKiemController.cs
@@ -4,14 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using EducationCode.Models;
 
 namespace EducationCode.Controllers
 {
-    public class TimKiem : Controller
+    public class TimKiemController : Controller
     {
+        // GET: TimKiem
         CODESHARKEntities db = new CODESHARKEntities();
-        // GET: BinhLuan
         public ActionResult TimKiem(string tukhoa)
         {
             List<BAIHOC> bh = db.BAIHOCs.Where(n => n.TENBAIHOC.Contains(tukhoa)).ToList();
