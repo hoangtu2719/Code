e442e5474137162cc31268cff1e7c83d79f4d469
Tu Luu <55480631+hoangtu2719@users.noreply.github.com>
2020-07-03 13:13:41 +0700

    add module tra loi

---
+++ EducationCode/Controllers/TraLoiController.cs
@@ -0,0 +1,24 @@
+﻿using EducationCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EducationCode.Controllers
+{
+    public class TraLoiController : Controller
+    {
+        // GET: TraLoi
+        CODESHARKEntities db = new CODESHARKEntities();
+        public ActionResult TraLoi(TRALOI tl)
+        {
+            NGUOIDUNG nd = (NGUOIDUNG)Session["NguoiDung"];
+            tl.NGAYDANG = DateTime.Now;
+            tl.MANGUOIDUNG = nd.MANGUOIDUNG;
+            db.TRALOIs.Add(tl);
+            db.SaveChanges();
+            return Redirect("/TrangChu/Index");
+        }
+    }
+}
